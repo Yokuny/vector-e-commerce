d0bc3184ecb0e021aa0ff4e8f0965bc920ae5f62
yokuny <felipe.vni@hotmail.com>
2023-04-17 10:01:59 -0300

    beggin

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
-  )
+  );
 }
